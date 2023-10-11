0e7e1ae19efb7c431ad3793d6d20a78063920d00
hithereryan <extmac@gmail.com>
2023-10-10 18:52:27 -0700

    navbar

--- app/page.js
+++ app/page.js
@@ -1,4 +1,36 @@
 export default function Home() {
-  return <p>Hello World!</p>
+  return <div>
+    <Navbar />
+    <Navbar />
+    <Navbar />
+    <Navbar />
+    <Navbar />
+    <Navbar />
+    <Navbar />
+    <Navbar />
+    </div>
+}
 
+function Navbar() {
+  return (
+    <nav className="border-b border-gray-800 sticky top-0 bg-gray-900 text-gray-100 z-10">
+      <div className="h-14 max-w-7x1 p-4 mx-auto flex items-center justify-between">
+        <a href="/" className="font-medium text-lg md:hover:underline">
+          My website
+        </a>
+        <ul className="flex items-center justify-end space-x-4">
+          <li>
+            <a href="/blog">
+              Blog
+            </a>
+          </li>
+          <li>
+            <a href="/photos">
+              Photos
+            </a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
 }
